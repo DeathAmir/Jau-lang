f27bea48cade3680cfb1f06db23c809b46413e61
DeathAmir <promoly1234@gmail.com>
2026-08-16 15:51:34 +0330

    Use a pure Jau library fixture for static archive tests

--- tests/multi_static.sh
+++ tests/multi_static.sh
@@ -8,7 +8,7 @@ trap 'rm -rf "$TMP"' EXIT
 c++ -std=c++17 -O2 -c "$ROOT/tests/multi_native_left.cpp" -o "$TMP/left.o"
 c++ -std=c++17 -O2 -c "$ROOT/tests/multi_native_right.cpp" -o "$TMP/right.o"
 
-"$JAUC" static "$ROOT/tests/multi_native.jau" -o "$TMP/libmulti.a" --target linux-x86_64 \
+"$JAUC" static "$ROOT/tests/multi_library.jau" -o "$TMP/libmulti.a" --target linux-x86_64 \
   --link "$TMP/left.o" \
   --link "$TMP/right.o"
 
