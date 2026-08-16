c5f6fbe286f4bfbd318915a539ed12c4a85b84ff
DeathAmir <promoly1234@gmail.com>
2026-08-16 15:27:36 +0330

    Export VM FFI fixture symbols on Windows

--- tests/vm_ffi.cpp
+++ tests/vm_ffi.cpp
@@ -1,9 +1,15 @@
 #include <cstdint>
 
-extern "C" std::intptr_t vm_add(std::intptr_t a, std::intptr_t b) {
+#ifdef _WIN32
+#define JAU_FFI_EXPORT extern "C" __declspec(dllexport)
+#else
+#define JAU_FFI_EXPORT extern "C" __attribute__((visibility("default")))
+#endif
+
+JAU_FFI_EXPORT std::intptr_t vm_add(std::intptr_t a, std::intptr_t b) {
     return a + b;
 }
 
-extern "C" std::intptr_t vm_mul(std::intptr_t a, std::intptr_t b) {
+JAU_FFI_EXPORT std::intptr_t vm_mul(std::intptr_t a, std::intptr_t b) {
     return a * b;
 }
